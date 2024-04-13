ab0feac825a0cab621961f7339955de6169df1c6
union-NandM <73626946+union-NandM@users.noreply.github.com>
2024-04-14 02:25:20 +0900

    chore: 設定の変更等

--- vite.config.ts
+++ vite.config.ts
@@ -3,14 +3,11 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react({
-      jsxImportSource: "@emotion/react",
-    }),
-  ],
+  plugins: [],
   resolve: {
     alias: {
       "@": "/src",
     },
   },
+  base: "/<REPO>/",
 });
