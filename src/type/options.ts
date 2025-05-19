7b6980e61527b27c50a6969ae4cc14fa81c5beed
union-NandM <73626946+union-NandM@users.noreply.github.com>
2025-05-19 13:42:44 +0900

    とりあえず倒立顕微鏡対応

--- src/type/options.ts
+++ src/type/options.ts
@@ -1,4 +1,13 @@
-export const OBJLENS_OPTIONS = ["x40", "x100", "x200", "x400", "x500"] as const;
+export const OBJLENS_OPTIONS = [
+  "x40",
+  "x100",
+  "x200",
+  "x400",
+  "x500",
+  "x100_inverted",
+  "x200_inverted",
+  "x400_inverted",
+] as const;
 export type ObjlensOption = (typeof OBJLENS_OPTIONS)[number];
 export const SCALEBAR_COLOR_OPTIONS = ["white", "black"] as const;
 export type ScalebarColorOption = (typeof SCALEBAR_COLOR_OPTIONS)[number];
