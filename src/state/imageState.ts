81b66707f5bb9927722bd9e4b8979a7c675b991c
union-NandM <73626946+union-NandM@users.noreply.github.com>
2025-05-26 00:46:27 +0900

    feat:倒立顕微鏡対応

--- src/state/imageState.ts
+++ src/state/imageState.ts
@@ -1,23 +1,37 @@
 import {
-  DEFAULT_MAGNIFICATION_CONFIG,
+  DEFAULT_SCALEBAR_LENGTH,
   DEFAULT_SCALEBAR_STATE,
 } from "@/constant/config";
-import { LoadedImage, Magnification, Scalebar } from "@/type/imageState";
+import { LoadedImage, Scalebar } from "@/type/imageState";
+import {
+  InvertedObjlensOption,
+  MagnificationConfig,
+  MicroscopeType,
+  UprightObjlensOption,
+} from "@/type/options";
+import {
+  isInvertedObjlensOption,
+  isSafeLoadedImage,
+  isUprightObjlensOption,
+} from "@/util/judgeMicrosocpeType";
 import { atom } from "jotai";
 
 export const imageAtom = atom<LoadedImage[]>([]);
 export const imageUpdaterAtom = atom(
   null,
-  (get, set, id: Symbol, newImageItems: Partial<LoadedImage>) => {
+  (get, set, newImageItems: Partial<LoadedImage>, id: Symbol) => {
     const image = get(imageAtom).find((element) => element.id === id);
     if (!image) return;
     set(imageAtom, (prev) =>
       prev.map((item) => {
         if (item.id !== id) return item;
-        return {
+        const newItem = {
           ...item,
           ...newImageItems,
         };
+        if (isSafeLoadedImage(newItem)) {
+          return newItem;
+        } else return item;
       })
     );
   }
@@ -32,6 +46,12 @@ export const imageDeleterAtom = atom(null, (get, set, id: Symbol) => {
 });
 
 export const selectedIdAtom = atom<Symbol | undefined>(undefined);
+export const selectedImageAtom = atom<LoadedImage | undefined>((get) => {
+  const loadedImages = get(imageAtom);
+  const selectedId = get(selectedIdAtom);
+  const image = loadedImages.find((element) => element.id === selectedId);
+  return image;
+});
 
 const getFromLocalStorage = (): Scalebar | null => {
   const value: string | null = localStorage.getItem("scalebar");
@@ -51,6 +71,28 @@ export const scalebarUpdaterAtom = atom(
   }
 );
 
-export const magnificationConfigAtom = atom<Magnification>({
-  ...DEFAULT_MAGNIFICATION_CONFIG,
+export const scalebarLengthAtom = atom<MagnificationConfig<number>>({
+  ...DEFAULT_SCALEBAR_LENGTH,
 });
+
+export const scalebarLengthUpdaterAtom = atom(
+  null,
+  (
+    _,
+    set,
+    microscopeType: MicroscopeType,
+    objlens: UprightObjlensOption | InvertedObjlensOption,
+    length: number
+  ) => {
+    set(scalebarLengthAtom, (prev) => {
+      const clone = structuredClone(prev);
+      if (microscopeType === "upright" && isUprightObjlensOption(objlens)) {
+        clone["upright"][objlens] = length;
+      }
+      if (microscopeType === "inverted" && isInvertedObjlensOption(objlens)) {
+        clone["inverted"][objlens] = length;
+      }
+      return clone;
+    });
+  }
+);
---
+++ src/util/judgeMicrosocpeType.ts
@@ -0,0 +1,63 @@
+import { LoadedImage, MagnificationOption } from "@/type/imageState";
+import {
+  INVERTED_OBJLENS_OPTIONS,
+  InvertedObjlensOption,
+  MICROSCOPE_TYPES,
+  MicroscopeType,
+  ObjlensOption,
+  ScalebarColorOption,
+  UPRIGHT_OBJLENS_OPTIONS,
+  UprightObjlensOption,
+} from "@/type/options";
+
+export const isUprightObjlensOption = (
+  objlens: string
+): objlens is UprightObjlensOption => {
+  return UPRIGHT_OBJLENS_OPTIONS.some((item) => item === objlens);
+};
+
+export const isInvertedObjlensOption = (
+  objlens: string
+): objlens is InvertedObjlensOption => {
+  return INVERTED_OBJLENS_OPTIONS.some((item) => item === objlens);
+};
+
+export const isMicroscopeType = (str: string): str is MicroscopeType => {
+  return MICROSCOPE_TYPES.some((item) => item === str);
+};
+
+export const isSafeMTypeOLPair = (obj: {
+  microscopeType: string;
+  objLens: string;
+}): obj is MagnificationOption => {
+  if (!isMicroscopeType(obj.microscopeType)) return false;
+  switch (obj.microscopeType) {
+    case "upright": {
+      return isUprightObjlensOption(obj.objLens);
+    }
+    case "inverted": {
+      return isInvertedObjlensOption(obj.objLens);
+    }
+  }
+};
+
+export type IncompleteLoadedImage = {
+  id: Symbol;
+  image: HTMLImageElement;
+  name: string;
+  color: ScalebarColorOption;
+} & {
+  length?: number;
+} & {
+  microscopeType: MicroscopeType;
+  objLens: ObjlensOption;
+};
+
+export const isSafeLoadedImage = (
+  ili: IncompleteLoadedImage
+): ili is LoadedImage => {
+  return isSafeMTypeOLPair({
+    microscopeType: ili.microscopeType,
+    objLens: ili.objLens,
+  });
+};
